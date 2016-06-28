128eb4cb227591db8797f3dd9228691e6c51cc9a
dianalimnos <dianalim1117@gmail.com>
2016-06-28 14:20:46 -0700

    Completed MakeSchoolNotes

---
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ListNotesTableViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ListNotesTableViewController: UITableViewController {
+    
+    var notes : Results<Note>! {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    @IBAction func unwindToListNotesViewController(segue: UIStoryboardSegue) {
+        
+        // for now, simply defining the method is sufficient.
+        // we'll add code later
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        notes = RealmHelper.retrieveNotes()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath) as! ListNotesTableViewCell
+        
+        // 1
+        let row = indexPath.row
+        
+        // 2
+        let note = notes[row]
+        
+        // 3
+        cell.noteTitleLabel.text = note.title
+        
+        // 4
+        cell.noteModificationTimeLabel.text = note.modificationTime.convertToString()
+        
+        return cell
+    }
+    // 1
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == .Delete {
+            //1
+            RealmHelper.deleteNote(notes[indexPath.row])
+            //2
+            notes = RealmHelper.retrieveNotes()
+        }
+    }
+}
